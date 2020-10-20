d7631ca4f662869f0f79a0f2baf11bf325773e84
Rahul Kumawat <rahulkkumawat2002@gmail.com>
2020-10-21 00:22:56 +0530

    modified 16

--- os_code/Stage15/batch.bat
+++ os_code/Stage15/batch.bat
@@ -1,13 +1,13 @@
-load --os $HOME/myexpos/os_code/Stage15/os_start.xsm
-load --init $HOME/myexpos/os_code/Stage15/even.xsm
-load --exec $HOME/myexpos/os_code/Stage15/odd.xsm
-load --idle $HOME/myexpos/os_code/Stage15/idle.xsm
-load --module 0 $HOME/myexpos/os_code/Stage15/resource_manager_module.xsm
-load --module 4 $HOME/myexpos/os_code/Stage15/device_manager_module.xsm
-load --module 7 $HOME/myexpos/os_code/Stage15/boot_module.xsm
-load --int=10 $HOME/myexpos/os_code/Stage15/exit_int.xsm
-load --exhandler $HOME/myexpos/os_code/Stage15/halt_prog.xsm
+load --os $HOME/myexpos/os_code/Stage15cc/os_start.xsm
+load --init $HOME/myexpos/os_code/Stage15cc/even.xsm
+load --exec $HOME/myexpos/os_code/Stage15cc/odd.xsm
+load --idle $HOME/myexpos/os_code/Stage15cc/idle.xsm
+load --module 0 $HOME/myexpos/os_code/Stage15cc/resource_manager_module.xsm
+load --module 4 $HOME/myexpos/os_code/Stage15cc/device_manager_module.xsm
+load --module 7 $HOME/myexpos/os_code/Stage15cc/boot_module.xsm
+load --int=10 $HOME/myexpos/os_code/Stage15cc/exit_int.xsm
+load --exhandler $HOME/myexpos/os_code/Stage15cc/halt_prog.xsm
 load --library $HOME/myexpos/expl/library.lib
-load --int=timer $HOME/myexpos/os_code/Stage15/timer_int.xsm
-load --int=7 $HOME/myexpos/os_code/Stage15/write_int.xsm
-load --module 5 $HOME/myexpos/os_code/Stage15/schedular_module.xsm
+load --int=timer $HOME/myexpos/os_code/Stage15cc/timer_int.xsm
+load --int=7 $HOME/myexpos/os_code/Stage15cc/write_int.xsm
+load --module 5 $HOME/myexpos/os_code/Stage15cc/schedular_module.xsm
--- xfs-interface/batch.bat
+++ xfs-interface/batch.bat
@@ -1,10 +1,14 @@
-load --os $HOME/myexpos/os_code/Stage13/ASS1/os_start.xsm
-load --init $HOME/myexpos/os_code/Stage13/ASS1/even.xsm
-load --exec $HOME/myexpos/os_code/Stage13/ASS1/odd.xsm
-load --idle $HOME/myexpos/os_code/Stage13/ASS1/idle.xsm
-load --module 7 $HOME/myexpos/os_code/Stage13/ASS1/boot_module.xsm
-load --int=10 $HOME/myexpos/os_code/Stage13/ASS1/halt_prog.xsm
-load --exhandler $HOME/myexpos/os_code/Stage13/ASS1/halt_prog.xsm
+load --os $HOME/myexpos/os_code/Stage16/os_start.xsm
+load --init $HOME/myexpos/os_code/Stage16/gcd.xsm
+load --idle $HOME/myexpos/os_code/Stage16/idle.xsm
+load --module 0 $HOME/myexpos/os_code/Stage16/resource_manager_module.xsm
+load --module 4 $HOME/myexpos/os_code/Stage16/device_manager_module.xsm
+load --module 7 $HOME/myexpos/os_code/Stage16/boot_module.xsm
+load --int=10 $HOME/myexpos/os_code/Stage16/exit_int.xsm
+load --exhandler $HOME/myexpos/os_code/Stage16/halt_prog.xsm
 load --library $HOME/myexpos/expl/library.lib
-load --int=timer $HOME/myexpos/os_code/Stage13/ASS1/timer_int.xsm
-load --int=7 $HOME/myexpos/os_code/Stage13/ASS1/write_int.xsm
+load --int=timer $HOME/myexpos/os_code/Stage16/timer_int.xsm
+load --int=7 $HOME/myexpos/os_code/Stage16/write_int.xsm
+load --module 5 $HOME/myexpos/os_code/Stage16/schedular_module.xsm
+load --int=6 $HOME/myexpos/os_code/Stage16/read_int.xsm
+load --int=console $HOME/myexpos/os_code/Stage16/console_int.xsm
