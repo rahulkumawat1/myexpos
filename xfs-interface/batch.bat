02431f4b88935838f14ed37e7c51eed562fe831b
Rahul Kumawat <rahulkkumawat2002@gmail.com>
2020-10-21 17:28:06 +0530

    Added Stage 17

--- xfs-interface/batch.bat
+++ xfs-interface/batch.bat
@@ -1,14 +1,17 @@
-load --os $HOME/myexpos/os_code/Stage16/os_start.xsm
-load --init $HOME/myexpos/os_code/Stage16/gcd.xsm
-load --idle $HOME/myexpos/os_code/Stage16/idle.xsm
-load --module 0 $HOME/myexpos/os_code/Stage16/resource_manager_module.xsm
-load --module 4 $HOME/myexpos/os_code/Stage16/device_manager_module.xsm
-load --module 7 $HOME/myexpos/os_code/Stage16/boot_module.xsm
-load --int=10 $HOME/myexpos/os_code/Stage16/exit_int.xsm
-load --exhandler $HOME/myexpos/os_code/Stage16/halt_prog.xsm
+load --os $HOME/myexpos/os_code/Stage17/os_start.xsm
 load --library $HOME/myexpos/expl/library.lib
-load --int=timer $HOME/myexpos/os_code/Stage16/timer_int.xsm
-load --int=7 $HOME/myexpos/os_code/Stage16/write_int.xsm
-load --module 5 $HOME/myexpos/os_code/Stage16/schedular_module.xsm
-load --int=6 $HOME/myexpos/os_code/Stage16/read_int.xsm
-load --int=console $HOME/myexpos/os_code/Stage16/console_int.xsm
+load --exhandler $HOME/myexpos/os_code/Stage17/halt_prog.xsm
+load --idle $HOME/myexpos/os_code/Stage17/idle.xsm
+load --init $HOME/myexpos/os_code/Stage17/gcd.xsm
+load --module 0 $HOME/myexpos/os_code/Stage17/resource_manager_module.xsm
+load --module 1 $HOME/myexpos/os_code/Stage17/process_man_mod.xsm
+load --module 2 $HOME/myexpos/os_code/Stage17/memory_man_mod.xsm
+load --module 4 $HOME/myexpos/os_code/Stage17/device_manager_module.xsm
+load --module 5 $HOME/myexpos/os_code/Stage17/schedular_module.xsm
+load --module 7 $HOME/myexpos/os_code/Stage17/boot_module.xsm
+load --int=6 $HOME/myexpos/os_code/Stage17/read_int.xsm
+load --int=7 $HOME/myexpos/os_code/Stage17/write_int.xsm
+load --int=9 $HOME/myexpos/os_code/Stage17/exec_int.xsm
+load --int=10 $HOME/myexpos/os_code/Stage17/exit_int.xsm
+load --int=timer $HOME/myexpos/os_code/Stage17/timer_int.xsm
+load --int=console $HOME/myexpos/os_code/Stage17/console_int.xsm
