0d88fa29a9e9f990d92e645cd197a859a77f8a75
Rahul Kumawat <rahulkkumawat2002@gmail.com>
2020-11-19 20:29:38 +0530

    included assign in Stage22

---
+++ os_code/Stage22_inc_assign/batch.bat
@@ -0,0 +1,31 @@
+load --os $HOME/myexpos/os_code/Stage22_inc_assign/os_start.xsm
+load --library $HOME/myexpos/expl/library.lib
+load --exhandler $HOME/myexpos/os_code/Stage22_inc_assign/excep_handler.xsm
+load --idle $HOME/myexpos/os_code/Stage22_inc_assign/idle.xsm
+load --init $HOME/myexpos/os_code/Stage22_inc_assign/shell.xsm
+load --exec $HOME/myexpos/os_code/Stage22_inc_assign/child.xsm
+load --exec $HOME/myexpos/os_code/Stage22_inc_assign/assg1.xsm
+load --exec $HOME/myexpos/os_code/Stage22_inc_assign/parent.xsm
+load --exec $HOME/myexpos/os_code/Stage22_inc_assign/odd.xsm
+load --exec $HOME/myexpos/os_code/Stage22_inc_assign/even.xsm
+load --exec $HOME/myexpos/os_code/Stage22_inc_assign/pid.xsm
+load --exec /home/rahul/myexpos/os_code/Stage22_inc_assign/conMerge.xsm
+load --exec /home/rahul/myexpos/os_code/Stage22_inc_assign/seqMerge.xsm
+load --module 0 $HOME/myexpos/os_code/Stage22_inc_assign/resource_manager_module.xsm
+load --module 1 $HOME/myexpos/os_code/Stage22_inc_assign/process_man_mod.xsm
+load --module 2 $HOME/myexpos/os_code/Stage22_inc_assign/memory_man_mod.xsm
+load --module 4 $HOME/myexpos/os_code/Stage22_inc_assign/device_manager_module.xsm
+load --module 5 $HOME/myexpos/os_code/Stage22_inc_assign/schedular_module.xsm
+load --module 7 $HOME/myexpos/os_code/Stage22_inc_assign/boot_module.xsm
+load --int=6 $HOME/myexpos/os_code/Stage22_inc_assign/read_int.xsm
+load --int=7 $HOME/myexpos/os_code/Stage22_inc_assign/write_int.xsm
+load --int=8 $HOME/myexpos/os_code/Stage22_inc_assign/fork_int.xsm
+load --int=9 $HOME/myexpos/os_code/Stage22_inc_assign/exec_int.xsm
+load --int=10 $HOME/myexpos/os_code/Stage22_inc_assign/exit_int.xsm
+load --int=11 $HOME/myexpos/os_code/Stage22_inc_assign/int11.xsm
+load --int=13 $HOME/myexpos/os_code/Stage22_inc_assign/int13.xsm
+load --int=14 $HOME/myexpos/os_code/Stage22_inc_assign/int14.xsm
+load --int=15 $HOME/myexpos/os_code/Stage22_inc_assign/shutdown_int.xsm
+load --int=timer $HOME/myexpos/os_code/Stage22_inc_assign/timer_int.xsm
+load --int=console $HOME/myexpos/os_code/Stage22_inc_assign/console_int.xsm
+load --int=disk $HOME/myexpos/os_code/Stage22_inc_assign/disk_int.xsm
--- xfs-interface/batch.bat
+++ xfs-interface/batch.bat
@@ -1,31 +1,31 @@
-load --os $HOME/myexpos/os_code/Stage22/os_start.xsm
+load --os $HOME/myexpos/os_code/Stage22_inc_assign/os_start.xsm
 load --library $HOME/myexpos/expl/library.lib
-load --exhandler $HOME/myexpos/os_code/Stage22/excep_handler.xsm
-load --idle $HOME/myexpos/os_code/Stage22/idle.xsm
-load --init $HOME/myexpos/os_code/Stage22/shell.xsm
-load --exec $HOME/myexpos/os_code/Stage22/child.xsm
-load --exec $HOME/myexpos/os_code/Stage22/assg1.xsm
-load --exec $HOME/myexpos/os_code/Stage22/parent.xsm
-load --exec $HOME/myexpos/os_code/Stage22/odd.xsm
-load --exec $HOME/myexpos/os_code/Stage22/even.xsm
-load --exec $HOME/myexpos/os_code/Stage22/pid.xsm
-load --exec /home/rahul/myexpos/os_code/Stage22/conMerge.xsm
-load --exec /home/rahul/myexpos/os_code/Stage22/seqMerge.xsm
-load --module 0 $HOME/myexpos/os_code/Stage22/resource_manager_module.xsm
-load --module 1 $HOME/myexpos/os_code/Stage22/process_man_mod.xsm
-load --module 2 $HOME/myexpos/os_code/Stage22/memory_man_mod.xsm
-load --module 4 $HOME/myexpos/os_code/Stage22/device_manager_module.xsm
-load --module 5 $HOME/myexpos/os_code/Stage22/schedular_module.xsm
-load --module 7 $HOME/myexpos/os_code/Stage22/boot_module.xsm
-load --int=6 $HOME/myexpos/os_code/Stage22/read_int.xsm
-load --int=7 $HOME/myexpos/os_code/Stage22/write_int.xsm
-load --int=8 $HOME/myexpos/os_code/Stage22/fork_int.xsm
-load --int=9 $HOME/myexpos/os_code/Stage22/exec_int.xsm
-load --int=10 $HOME/myexpos/os_code/Stage22/exit_int.xsm
-load --int=11 $HOME/myexpos/os_code/Stage22/int11.xsm
-load --int=13 $HOME/myexpos/os_code/Stage22/int13.xsm
-load --int=14 $HOME/myexpos/os_code/Stage22/int14.xsm
-load --int=15 $HOME/myexpos/os_code/Stage22/shutdown_int.xsm
-load --int=timer $HOME/myexpos/os_code/Stage22/timer_int.xsm
-load --int=console $HOME/myexpos/os_code/Stage22/console_int.xsm
-load --int=disk $HOME/myexpos/os_code/Stage22/disk_int.xsm
+load --exhandler $HOME/myexpos/os_code/Stage22_inc_assign/excep_handler.xsm
+load --idle $HOME/myexpos/os_code/Stage22_inc_assign/idle.xsm
+load --init $HOME/myexpos/os_code/Stage22_inc_assign/shell.xsm
+load --exec $HOME/myexpos/os_code/Stage22_inc_assign/child.xsm
+load --exec $HOME/myexpos/os_code/Stage22_inc_assign/assg1.xsm
+load --exec $HOME/myexpos/os_code/Stage22_inc_assign/parent.xsm
+load --exec $HOME/myexpos/os_code/Stage22_inc_assign/odd.xsm
+load --exec $HOME/myexpos/os_code/Stage22_inc_assign/even.xsm
+load --exec $HOME/myexpos/os_code/Stage22_inc_assign/pid.xsm
+load --exec /home/rahul/myexpos/os_code/Stage22_inc_assign/conMerge.xsm
+load --exec /home/rahul/myexpos/os_code/Stage22_inc_assign/seqMerge.xsm
+load --module 0 $HOME/myexpos/os_code/Stage22_inc_assign/resource_manager_module.xsm
+load --module 1 $HOME/myexpos/os_code/Stage22_inc_assign/process_man_mod.xsm
+load --module 2 $HOME/myexpos/os_code/Stage22_inc_assign/memory_man_mod.xsm
+load --module 4 $HOME/myexpos/os_code/Stage22_inc_assign/device_manager_module.xsm
+load --module 5 $HOME/myexpos/os_code/Stage22_inc_assign/schedular_module.xsm
+load --module 7 $HOME/myexpos/os_code/Stage22_inc_assign/boot_module.xsm
+load --int=6 $HOME/myexpos/os_code/Stage22_inc_assign/read_int.xsm
+load --int=7 $HOME/myexpos/os_code/Stage22_inc_assign/write_int.xsm
+load --int=8 $HOME/myexpos/os_code/Stage22_inc_assign/fork_int.xsm
+load --int=9 $HOME/myexpos/os_code/Stage22_inc_assign/exec_int.xsm
+load --int=10 $HOME/myexpos/os_code/Stage22_inc_assign/exit_int.xsm
+load --int=11 $HOME/myexpos/os_code/Stage22_inc_assign/int11.xsm
+load --int=13 $HOME/myexpos/os_code/Stage22_inc_assign/int13.xsm
+load --int=14 $HOME/myexpos/os_code/Stage22_inc_assign/int14.xsm
+load --int=15 $HOME/myexpos/os_code/Stage22_inc_assign/shutdown_int.xsm
+load --int=timer $HOME/myexpos/os_code/Stage22_inc_assign/timer_int.xsm
+load --int=console $HOME/myexpos/os_code/Stage22_inc_assign/console_int.xsm
+load --int=disk $HOME/myexpos/os_code/Stage22_inc_assign/disk_int.xsm
